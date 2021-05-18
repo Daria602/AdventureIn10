54aa90533c6963c853882ec47920d4fc9ec558cc
Daria602 <73795793+Daria602@users.noreply.github.com>
2021-05-18 08:18:00 +0300

    Update Inventory.h

--- classes/Inventory.h
+++ classes/Inventory.h
@@ -9,7 +9,7 @@ public:
 	Inventory() { this->backpack = {}; }
 	virtual ~Inventory(){}
 
-	std::vector<Edible> const getInventory() { return this->backpack; }
+	std::vector<Edible> getInventory() { return this->backpack; }
 	void addToInventory(Edible something);
 	void eraseFromInventory(int index);
 	void inInventory(Player &player);
