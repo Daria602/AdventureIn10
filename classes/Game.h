274a10267744f12fca56805a00c29ba9149d51ca
Daria602 <73795793+Daria602@users.noreply.github.com>
2021-05-18 08:17:39 +0300

    Update Game.h

--- classes/Game.h
+++ classes/Game.h
@@ -11,8 +11,8 @@ public:
 	Game() { this->running = true; this->dayCount = 1; }
 	virtual ~Game() {}
 
-	bool const getRunning() { return this->running; }
-	int const getDayCount() { return this->dayCount; }
+	bool getRunning() { return this->running; }
+	int getDayCount() { return this->dayCount; }
 
 	void setRunningFalse() { this->running = false; }
 	void incrementDayCount() { this->dayCount++; std::cout << "You rested. Starting Day " << this->dayCount << std::endl; }
